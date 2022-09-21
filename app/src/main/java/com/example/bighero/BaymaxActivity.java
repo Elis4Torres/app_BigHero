23b6c2150e68860a7ec8e921f88f0dab9facaa1e
Elis4Torres <elis.torres@etec.sp.gov.br>
2022-09-21 18:52:59 -0300

    ativando função do sensor

--- app/src/main/java/com/example/bighero/BaymaxActivity.java
+++ app/src/main/java/com/example/bighero/BaymaxActivity.java
@@ -1,17 +1,80 @@
 package com.example.bighero;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+
+import android.graphics.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class BaymaxActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor sensorManagerAproximidade;
+    private SensorEventListener sensorAproximidadeListener;
+
+
+    boolean laterna_bool = false;
+    CameraManager sensorManagerLaterna;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baymax);
+        sensorManagerLaterna = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManagerAproximidade = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        sensorAproximidadeListener = new SensorEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0] < sensorManagerAproximidade.getMaximumRange()){
+                    try {
+                        sensorManagerLaterna.setTorchMode("0", true);
+                        laterna_bool = true;
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        sensorManagerLaterna.setTorchMode("0", false);
+                        laterna_bool = false;
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) { }
+        };
+        sensorManager.registerListener(sensorAproximidadeListener, sensorManagerAproximidade, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void  onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(sensorAproximidadeListener);
     }
 
     public void Back(View view){
